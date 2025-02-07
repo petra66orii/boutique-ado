c7d69314449da617ff6ceba122e62d06b38c6b4c
petra66orii <petra66orii@gmail.com>
2025-02-07 19:16:27 +0000

    Implement code for adding and/or removing products

--- products/models.py
+++ products/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Category(models.Model):
 
@@ -23,18 +21,17 @@ class Product(models.Model):
         'Category',
         null=True,
         blank=True,
-        on_delete=models.SET_NULL
-        )
+        on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6,
         decimal_places=2,
         null=True,
-        blank=True
-        )
+        blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
+    path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
