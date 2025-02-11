ff0a23fc28e243d0f934b4aa7a960c839c0ea8e3
petra66orii <petra66orii@gmail.com>
2025-02-11 12:38:09 +0000

    Add toasts to shopping bag

--- products/views.py
+++ products/views.py
@@ -31,7 +31,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -40,15 +40,10 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(
-                    request,
-                    "You didn't enter any search criteria!"
-                    )
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = (
-                Q(name__icontains=query) | Q(description__icontains=query)
-                )
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
