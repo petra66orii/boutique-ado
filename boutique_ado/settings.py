dd7b0cfb34618eb1fc42a5ef93ae7929dbcd14f0
petra66orii <petra66orii@gmail.com>
2025-02-13 21:39:39 +0000

    Add webhooks handlers

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -32,7 +32,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-petra66orii-boutiqueado-4fy9ix5spo5.ws.codeinstitute-ide.net',
-    '127.0.0.1'
+    '127.0.0.1',
+    'localhost'
 ]
 
 
@@ -191,6 +192,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
